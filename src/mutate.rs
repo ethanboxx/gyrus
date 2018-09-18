5b89971576700798671fcd3d97377ae20661c589
Ethan Brierley <ethanboxx@gmail.com>
2018-09-18 20:48:05 +0100

    work

--- src/mutate.rs
+++ src/mutate.rs
@@ -1,32 +1,33 @@
-struct mutate_type {
+use crate::Gene;
+
+enum mutate_type {
     // Changes all kinds of values
     Strong,
     // Only cange values
     OnlyValues,
-    
 }
 
 impl Gene {
     pub fn mutate(&self) -> Self {
         let mut new_values = self.clone();
         if !self.validate() {
-            panic!("Genes not compatible. Can't breed.");
+            panic!("Genes not valid. Can't mutate.");
         };
         // Mutate nodes.
         for (node_line_index, node_line) in self.node_dna.iter().enumerate() {
             for (node_index, node) in node_line.iter().enumerate() {
-                new_values.node_dna[node_line_index][node_index] =
-                    node.node_mutate();
+                new_values.node_dna[node_line_index][node_index] = node.node_mutate();
             }
         }
         // Mutate lines.
         for (block_index, block) in self.line_dna.iter().enumerate() {
             for (node_point_index, node_point) in block.iter().enumerate() {
                 for (line_index, line) in node_point.iter().enumerate() {
-                    new_values.line_dna[block_index][node_point_index][line_index] = line
-                        .line_mutate()
+                    new_values.line_dna[block_index][node_point_index][line_index] =
+                        line.line_mutate()
                 }
             }
         }
-        new_values.clone()}
+        new_values.clone()
+    }
 }
