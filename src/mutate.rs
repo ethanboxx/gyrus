57e50c5d3919042c8e7f7689339fa0280f8d6a68
Ethan Brierley <ethanboxx@gmail.com>
2018-09-18 11:00:12 +0100

    make the func

---
+++ src/mutate.rs
@@ -0,0 +1,11 @@
+struct mutate_type {
+    // Changes all kinds of values
+    Strong,
+    // Only cange values
+    OnlyValues,
+    
+}
+
+impl Gene {
+    pub fn breed(&self, second_gene: &Self) -> Self {}
+}
