d52f13b5f5a9b89940773dc2af55168efc4f2f86
Ethan Brierley <ethanboxx@gmail.com>
2018-09-19 14:31:26 +0100

    what this needs is more code

--- src/generation.rs
+++ src/generation.rs
@@ -1,6 +1,27 @@
-use {chrono::prelude::Utc, crate::Gene};
+use {
+    chrono::{DateTime, Utc},
+    crate::Gene,
+};
 struct Generation {
     genes: Vec<Gene>,
+    date_created: Option<DateTime<Utc>>,
+    intended_size: u16,
+    generations_before: u64,
+}
 
-    date_created: Option<Utc>,
+impl Generation {
+    pub fn new_rand(size: u16) -> Self {
+        Self {
+            genes: {
+                let mut rand_vec = Vec::new();
+                for _x in 0..size {
+                    rand_vec.push(Gene::new_random_gene());
+                }
+                rand_vec
+            },
+            date_created: Some(Utc::now()),
+            intended_size: size,
+            generations_before: 0,
+        }
+    }
 }
