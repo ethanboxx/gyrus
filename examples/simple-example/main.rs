4c20736a2b455cd4413c506ca166a07368edd48a
Ethan Brierley <ethanboxx@gmail.com>
2018-09-20 12:38:42 +0100

    It works! time for clean up work

--- examples/simple-example/main.rs
+++ examples/simple-example/main.rs
@@ -24,15 +24,27 @@ use rayon::prelude::*;
 //TODO lets get some scores to se how easy this is
 
 fn main() {
-    let mut generation = Generation::new_rand(20, 2, 4);
+    let mut generation = Generation::new_rand(3, 2, 3);
     println!("Random start generation {:#?}", generation);
     generation = Generation {
         genes: generation
             .genes
-            .par_iter()
+            .iter()
             .map(|current| {
                 GeneScore {
-                    score: 10.0, //TODO Calc score form gene
+                    score: {
+                        let mut score = 0.0;
+                        if largest_of_3(&current.gene.clone().output(&[0, 1, 0])) == 1 {
+                            score = score + 1.0;
+                        }
+                        if largest_of_3(&current.gene.clone().output(&[1, 0, 0])) == 2 {
+                            score = score + 1.0;
+                        }
+                        if largest_of_3(&current.gene.clone().output(&[0, 0, 1])) == 0 {
+                            score = score + 1.0;
+                        }
+                        score
+                    }, //TODO Calc score form gene
                     ..current.clone()
                 }.clone()
             }).collect(),
@@ -40,4 +52,16 @@ fn main() {
         ..generation
     };
     println!("Random start generation {:#?}", generation);
+}
+
+fn largest_of_3(arr: &Vec<f64>) -> i8 {
+    if arr[0] > arr[1] && arr[0] > arr[2] {
+        0
+    } else if arr[1] > arr[0] && arr[1] > arr[2] {
+        1
+    } else if arr[2] > arr[0] && arr[2] > arr[1] {
+        2
+    } else {
+        0
+    }
 }
