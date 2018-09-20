236052a5809919426f28f0421680e5e514af91fc
Ethan Brierley <ethanboxx@gmail.com>
2018-09-20 10:12:33 +0100

    basic spec

---
+++ examples/simple-example/main.rs
@@ -0,0 +1,18 @@
+//To run example `cargo run --example simple-example --release`
+// Let user pick number of gen to process every time.
+
+// Take 3 inputs
+// Each one has 1 or 0 going through.
+
+// in   out
+// 0  |
+// 1  |  1
+// 0  |
+
+// 1  |
+// 0  |
+// 0  |  1
+
+// 0  |  1
+// 0  |
+// 1  |
