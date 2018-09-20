66d8c557919c61d89e801abd5751875a528029a7
Ethan Brierley <ethanboxx@gmail.com>
2018-09-20 21:29:24 +0100

    it works

--- examples/simple-example/main.rs
+++ examples/simple-example/main.rs
@@ -40,50 +40,58 @@ use rayon::prelude::*;
 //TODO lets get some scores to se how easy this is
 
 fn main() {
-    let mut generation = Generation::new_rand(80, 2, 3);
+    let mut generation = Generation::new_rand(40, 2, 3);
     // println!("Random start generation {:#?}", generation);
-    generation = Generation {
-        genes: generation
-            .genes
-            .par_iter()
-            .map(|current| {
-                GeneScore {
-                    score: {
-                        let mut score = 0.0;
-                        if largest_of_3(&current.gene.clone().output(&[0, 1, 0])) == 1 {
-                            score = score + 1.0;
-                        }
-                        if largest_of_3(&current.gene.clone().output(&[1, 0, 0])) == 2 {
-                            score = score + 1.0;
-                        }
-                        if largest_of_3(&current.gene.clone().output(&[0, 0, 1])) == 0 {
-                            score = score + 1.0;
-                        }
-                        if largest_of_3(&current.gene.clone().output(&[1, 1, 0])) == 1 {
-                            score = score + 1.0;
-                        }
-                        if largest_of_3(&current.gene.clone().output(&[1, 0, 1])) == 1 {
-                            score = score + 1.0;
-                        }
-                        if largest_of_3(&current.gene.clone().output(&[0, 1, 1])) == 2 {
-                            score = score + 1.0;
-                        }
-                        if largest_of_3(&current.gene.clone().output(&[2, 1, 1])) == 2 {
-                            score = score + 1.0;
-                        }
-                        score
-                    }, //TODO Calc score form gene
-                    ..current.clone()
-                }.clone()
-            }).collect(),
-        generations_before: generation.generations_before + 1,
-        ..generation
-    };
-    println!("Random middle generation {:#?}", generation);
-    println!("Random middle len {:#?}", generation.genes.len());
-    generation.update();
-    println!("Random end generation {:#?}", generation);
-    println!("Random end len {:#?}", generation.genes.len());
+    for x in 0..100 {
+        generation = Generation {
+            genes: generation
+                .genes
+                .par_iter()
+                .map(|current| {
+                    GeneScore {
+                        score: {
+                            let mut score = 0.0;
+                            if largest_of_3(&current.gene.clone().output(&[0, 1, 0])) == 1 {
+                                score = score + 1.0;
+                            }
+                            if largest_of_3(&current.gene.clone().output(&[1, 0, 0])) == 2 {
+                                score = score + 1.0;
+                            }
+                            if largest_of_3(&current.gene.clone().output(&[0, 0, 1])) == 0 {
+                                score = score + 1.0;
+                            }
+                            if largest_of_3(&current.gene.clone().output(&[1, 1, 0])) == 1 {
+                                score = score + 1.0;
+                            }
+                            if largest_of_3(&current.gene.clone().output(&[1, 0, 1])) == 1 {
+                                score = score + 1.0;
+                            }
+                            if largest_of_3(&current.gene.clone().output(&[0, 1, 1])) == 2 {
+                                score = score + 1.0;
+                            }
+                            if largest_of_3(&current.gene.clone().output(&[2, 1, 1])) == 2 {
+                                score = score + 1.0;
+                            }
+                            score
+                        }, //TODO Calc score form gene
+                        ..current.clone()
+                    }.clone()
+                }).collect(),
+            generations_before: generation.generations_before + 1,
+            ..generation
+        };
+        // println!("Random middle generation {:#?}", generation);
+        // println!("Random middle len {:#?}", generation.genes.len());
+
+        generation.update();
+        generation.sort();
+        println!(
+            "score top {}",
+            generation.genes[generation.genes.len() - 1].score
+        );
+        // println!("Random end generation {:#?}", generation);
+        // println!("Random end len {:#?}", generation.genes.len());
+    }
 }
 
 fn largest_of_3(arr: &Vec<f64>) -> i8 {
