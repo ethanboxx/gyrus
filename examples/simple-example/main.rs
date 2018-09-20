03580fc5b12ae451b168f858c29326f01ade61e5
Ethan Brierley <ethanboxx@gmail.com>
2018-09-20 11:52:51 +0100

    some hard code

--- examples/simple-example/main.rs
+++ examples/simple-example/main.rs
@@ -18,11 +18,26 @@
 // 1  |
 
 extern crate ai_graph;
+use ai_graph::generation::GeneScore;
 use ai_graph::generation::Generation;
-
+use rayon::prelude::*;
 //TODO lets get some scores to se how easy this is
 
 fn main() {
-    let generation = Generation::new_rand(20, 2, 4);
+    let mut generation = Generation::new_rand(20, 2, 4);
+    println!("Random start generation {:#?}", generation);
+    generation = Generation {
+        genes: generation
+            .genes
+            .par_iter()
+            .map(|current| {
+                GeneScore {
+                    score: 10.0, //TODO Calc score form gene
+                    ..current.clone()
+                }.clone()
+            }).collect(),
+        generations_before: generation.generations_before + 1,
+        ..generation
+    };
     println!("Random start generation {:#?}", generation);
 }
