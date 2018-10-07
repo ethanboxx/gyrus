f6ff79bf3d3890d1b6c90a20084e352804049add
Ethan Brierley <ethanboxx@gmail.com>
2018-10-07 20:14:14 +0100

    Rename ai-graph to gyrus

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(tool_lints)]
 #![warn(clippy::pedantic)]
-//! Ai Graph is a new tool for creating machine learning that runs blazingly fast when learning has finnished.
+//! Gyrus is a new tool for creating machine learning that runs blazingly fast when learning has finnished.
 
 pub mod generation;
 mod tests;
