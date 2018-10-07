cc9dd270135a8bfbe9c965602c9c1b1d8afe8b31
Ethan Brierley <ethanboxx@gmail.com>
2018-10-07 20:07:15 +0100

    remove old confusing comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-//TODO add ai graph with fixed number of inputs and outputs but costom layers
-//TODO More tests that check if the graph is vaild
-//TODO Consider changing file structure
-
 #![feature(tool_lints)]
 #![warn(clippy::pedantic)]
 //! Ai Graph is a new tool for creating machine learning that runs blazingly fast when learning has finnished.
