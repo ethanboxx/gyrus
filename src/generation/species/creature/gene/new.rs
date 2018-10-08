741fbd314fc3e25b8c7dceb2aa87cc75d0bf925d
Ethan Brierley <ethanboxx@gmail.com>
2018-10-08 12:47:51 +0100

    Remove magic numbers and replace with functions that can make any gene

--- src/generation/species/creature/gene/new.rs
+++ src/generation/species/creature/gene/new.rs
@@ -36,85 +36,44 @@ impl Gene {
             },
         }
     }
-    pub fn new_gene() -> Self {
-        Self {
-            line_dna: vec![vec![vec![MutationLine::Reset; 9]; 9]; 2],
-            node_dna: vec![vec![MutationNode::Add; 9]; 2],
-        }
-    }
     // Test new shape of gene
     pub fn new_gene_shape_test() -> Self {
         Self::new(4, 4, 16, 3)
-    }
-    pub fn new_gene_shape_test_shuffle() -> Self {
-        let mut x = Self::new_gene_shape_test();
-        x.shuffle();
-        x
-    }
-    pub fn new_random_gene() -> Self {
-        Self::new_random_basic_gene(2, 9)
-    }
-    pub fn new_random_basic_gene(depth: u8, hight: u8) -> Self {
-        Self {
-            line_dna: MutationLine::rand_vec3(depth, hight, hight),
-            node_dna: MutationNode::rand_vec2(depth, hight),
-        }
     }
     pub fn shuffle(&mut self) -> () {
         self.node_dna = self
             .node_dna
             .iter_mut()
-            .map(|x| x.iter_mut().map(|_| MutationNode::rand_mut()).collect())
+            .map(|x| x.iter_mut().map(|_| MutationNode::random()).collect())
             .collect();
         self.line_dna = self
             .line_dna
             .iter_mut()
             .map(|x| {
                 x.iter_mut()
-                    .map(|x| x.iter_mut().map(|_| MutationLine::rand_mut()).collect())
+                    .map(|x| x.iter_mut().map(|_| MutationLine::random()).collect())
                     .collect()
             })
             .collect();
     }
 }
 
-trait RandVec
+trait Random
 where
     Self: Sized,
 {
-    fn rand_mut() -> Self;
-    fn rand_vec3(num: u8, num2: u8, num3: u8) -> Vec<Vec<Vec<Self>>> {
-        let mut rand_vec = Vec::new();
-        for _ in 0..num {
-            rand_vec.push(Self::rand_vec2(num2, num3));
-        }
-        rand_vec
-    }
-    fn rand_vec2(num: u8, num2: u8) -> Vec<Vec<Self>> {
-        let mut rand_vec = Vec::new();
-        for _ in 0..num {
-            rand_vec.push(Self::rand_vec(num2));
-        }
-        rand_vec
-    }
-    fn rand_vec(num: u8) -> Vec<Self> {
-        let mut rand_vec = Vec::new();
-        for _ in 0..num {
-            rand_vec.push(Self::rand_mut());
-        }
-        rand_vec
-    }
+    fn random() -> Self;
 }
 
-impl RandVec for MutationNode {
-    fn rand_mut() -> Self {
+impl Random for MutationNode {
+    fn random() -> Self {
         let mut rng = rand::thread_rng();
         *rng.choose(&NODE_TYPES).unwrap()
     }
 }
 
-impl RandVec for MutationLine {
-    fn rand_mut() -> Self {
+impl Random for MutationLine {
+    fn random() -> Self {
         let mut rng = rand::thread_rng();
         let line_types = [
             MutationLine::Pass,
