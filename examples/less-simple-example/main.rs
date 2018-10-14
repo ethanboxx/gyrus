9c11a0057d1432d355a36b24e75b0929ffec9e84
Ethan Brierley <ethanboxx@gmail.com>
2018-10-14 17:37:46 +0100

    Remove lines no longer needed

--- examples/less-simple-example/main.rs
+++ examples/less-simple-example/main.rs
@@ -1,4 +1,3 @@
-#![feature(tool_lints)]
 #![warn(clippy::pedantic)]
 //To run example `cargo run --example less-simple-example --release`
 // Let user pick number of gen to process every time.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(tool_lints)]
 #![warn(clippy::pedantic)]
 //! Gyrus is a new tool for creating machine learning that runs blazingly fast when learning has finnished.
 
